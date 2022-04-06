8ab3c7095143d9acd47a8ca1ae6bfda1d68a7ab2
Jun Han <33768833+jhh02@users.noreply.github.com>
2022-04-06 08:18:16 -0700

    updated

--- scripts/enter.js
+++ scripts/enter.js
@@ -1,19 +1,3 @@
-let searchQuery = null;
-
-document.querySelector("#pac-input").addEventListener("keyup", function (e) {
-    if (e.keycode === 13 || e.key === "Enter") {
-        e.preventDefault();
-        searchQuery = e.target.value;
-        if (!searchQuery) {
-            const myModal = new bootstrap.Modal(document.getElementById('myModal'))
-            myModal.toggle();
-        } else {
-            document.querySelector('.landing-page').style.display = "none"
-            document.querySelector('.card-details').style.display = "grid"
-
-
-        }
-    }
-
+document.querySelector('.foodtruck-card').addEventListener("click", function () {
+    window.location.href = "description.html"
 })
-
